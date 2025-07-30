26c5e1d5db2760d53617723b274d758d1a9cc124
FlickDaWist <putuaugiokaadiana@gmail.com>
2025-07-30 11:34:17 +0800

    add google provider

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ terraform {
   required_version = ">=1.5"
   required_providers {
     google = {
-      project = var.project_id
-      region  = var.region
-      zone    = var.zone
+      source  = "hashicorp/google"
+      version = "~>6.56"
     }
   }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
