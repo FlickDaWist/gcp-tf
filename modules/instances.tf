d3fc536b50cdcfef6ae9fbcf583a8cc57766986e
FlickDaWist <putuaugiokaadiana@gmail.com>
2025-07-30 11:55:17 +0800

    import gc instances

---
+++ modules/instances.tf
@@ -0,0 +1,86 @@
+# resource "google_service_account" "default" {
+#   account_id   = "my-custom-sa"
+#   display_name = "Custom SA for VM Instance"
+# }
+resource "google_compute_instance" "tf-instance-1" {
+  name         = "tf-instance-1"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20250728"
+      #   labels = {
+      #     my_label = "value"
+      #   }
+    }
+  }
+
+  // Local SSD disk
+  #   scratch_disk {
+  #     interface = "NVME"
+  #   }
+
+
+  network_interface {
+    network = "nic0"
+
+    # access_config {
+    #   // Ephemeral public IP
+    # }
+  }
+
+
+  metadata_startup_script   = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+
+  #   service_account {
+  #     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+  #     email  = google_service_account.default.email
+  #     scopes = ["cloud-platform"]
+  #   }
+}
+
+
+resource "google_compute_instance" "tf-instance-2" {
+  name         = "tf-instance-2"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20250728"
+      #   labels = {
+      #     my_label = "value"
+      #   }
+    }
+  }
+
+  // Local SSD disk
+  #   scratch_disk {
+  #     interface = "NVME"
+  #   }
+
+
+  network_interface {
+    network = "nic0"
+
+    # access_config {
+    #   // Ephemeral public IP
+    # }
+  }
+
+
+  metadata_startup_script   = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+
+  #   service_account {
+  #     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+  #     email  = google_service_account.default.email
+  #     scopes = ["cloud-platform"]
+  #   }
+}
